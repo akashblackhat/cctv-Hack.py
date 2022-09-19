b016e45bc0f3b16f22b64ee2d98c380385bba1ed
AkashBlackHat <88341460+akashblackhat@users.noreply.github.com>
2022-09-19 20:36:59 +0530

    Add files via upload

---
+++ php.py
@@ -0,0 +1,23 @@
+
+slowprint(''' \033[93m
+[01] python             [02] python2
+[03] python-dev         [04] python3
+[05] php                [06] java
+[07] git                [08] perl
+[09] bash               [10] nano
+[11] curl               [12] openssl
+[13] openssh            [14] wget
+[15] clang              [16] nmap
+[17] w3m                [18] hydra
+[19] ruby               [20] macchanger
+[21] host               [22] dnsutils
+[23] coreutils          [24] fish
+[25] zip                [27] tor
+[28] hydra              [29] figlet 
+[30] cowsay             [31] tar
+[32] unzip              [33] vim
+[34] ruby               [35] wcalc
+[36] bmon               [37] unrar
+[38] proot              [39] golang
+[40] NsLookup''')
+slowprint('''\033[96m
